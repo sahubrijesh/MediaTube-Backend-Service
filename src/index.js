9e3e421c39c3b51c35b94fa1081e40523d85ebf5
BRIJESH SAHU <thebrijeshsahu@gmail.com>
2024-05-09 19:20:01 +0530

    util codes

--- src/index.js
+++ src/index.js
@@ -5,22 +5,16 @@ dotenv.config({
     path: "./.env",
 });
 
-connectDB();
-
-// import express from "express";
-// const app = express();
-
-// ( async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//         console.log("Connected to MongoDB"); 
-//         app.on("error", (error) => {
-//             console.error("ERROR: ", error);
-//             throw error
-//         })                                                                                                                                                                                                            
-//     }catch (error){
-//         console.error("ERROR: ", error);
-//         throw error
-//     }
-    
-// })()
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error: ", error);
+        process.exit(1);
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed: ", error);
+})
