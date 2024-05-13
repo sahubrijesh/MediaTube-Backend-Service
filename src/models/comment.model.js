88961afd9e0697124fc65c93205cb2dfd8179fed
BRIJESH SAHU <thebrijeshsahu@gmail.com>
2024-05-13 20:32:21 +0530

    added new models

---
+++ src/models/comment.model.js
@@ -0,0 +1,25 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAgregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: "Video",
+            required: true
+        }
+    },
+    {timestamps: true}
+)
+
+commentSchema.plugin(mongooseAgregatePaginate)
+export const Comment = mongoose.model("Comment", commentSchema)
