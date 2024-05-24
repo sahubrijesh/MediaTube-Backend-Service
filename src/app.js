5f3f7b8e29e5eb94b3fae120501b210a862c55fc
BRIJESH SAHU <thebrijeshsahu@gmail.com>
2024-05-25 01:21:42 +0530

    added tweet controller

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import tweetRouter from "./routes/tweet.route.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
@@ -28,5 +29,6 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/tweets", tweetRouter);
 
 export {app};
---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,119 @@
+import mongoose, { isValidObjectId } from "mongoose"
+import {Tweet} from "../models/tweet.model.js"
+import {User} from "../models/user.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const createTweet = asyncHandler(async (req, res) => {
+    //TODO: create tweet
+
+    const {content} = req.body
+
+
+    if(!content?.trim()) {
+        throw new ApiError(400, "Content is required")
+    }
+
+    const newTweet = await Tweet.create({
+        content,
+        owner: req.user._id
+    })
+    
+    return res
+    .status(201)
+    .json(new ApiResponse(201, newTweet, "Tweet created successfully"))
+})
+
+const getUserTweets = asyncHandler(async (req, res) => {
+    // TODO: get user tweets
+
+    const {page = 1, limit = 10} = req.query
+
+    const tweets = await Tweet
+    .find({owner: req.user._id})
+    .skip((page - 1) * limit)
+    .limit(limit)
+    .sort({createdAt: -1})
+    .populate({ path: "owner", select: "username" })
+    .lean()
+
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "Tweets fetched successfully"))
+})
+
+const updateTweet = asyncHandler(async (req, res) => {
+    //TODO: update tweet
+
+    const {tweetId} = req.params
+    const {content} = req.body
+
+    if(!tweetId?.trim()) {
+        throw new ApiError(400, "tweetId is required")
+    }
+
+    if(!content?.trim()) {  
+        throw new ApiError(400, "Content is required")
+    }       
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if(!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }   
+
+    if(tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to update this tweet")
+    }
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set: {
+                content
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updatedTweet, "Tweet updated successfully"))
+})
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    //TODO: delete tweet
+
+    const {tweetId} = req.params    
+
+    if(!tweetId?.trim()) {
+        throw new ApiError(400, "tweetId is required")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if(!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    if(tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to delete this tweet")
+    }
+
+    await Tweet.findByIdAndDelete(tweetId)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, null, "Tweet deleted successfully"))
+})
+
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet
+}
